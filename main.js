9e8afaa554508572765f3e8f76bb6963207a4753
Andrew Murray <darbyguitars@gmail.com>
2017-08-07 16:01:43 -0500

    almost there but not quite

--- main.js
+++ main.js
@@ -7,27 +7,29 @@ function requestListener() {
   let data = JSON.parse(this.responseText);
   console.log(data);
   let list = `
-<div class="header">
-  <h1>${data.name}</h1>
-</div>
-<section>
-<div class="basics">
-  <h2>The Basics</h2>
-  <ul>
-    <li><span class='blue' >Name:</span> ${data.name}</li>
-    <li>Github URL: ${data.url}</li>
-    <li>Email: darbyguitars@gmail.com</li>
-    <li>Company: ${data.company}</li>
-    <li>Website: ${data.blog}</li>
-  </ul>
-</div>
-</section>
-<div class="story">
-  <p>${data.bio}</p>
-</div>
-  <img src=${data.avatar_url} alt="">
-<div class="pic">
-</div>`;
+  <div class="header">
+    <h1>${data.name}</h1>
+  </div>
+  <section>
+  <div class="basics">
+    <h1>The Basics</h1>
+    <ul>
+      <li><span class='blue' >Name:</span> ${data.name}</li>
+      <li><span class="blue">Github URL:</span> ${data.url}</li>
+      <li><span class="blue">Email:</span> darbyguitars@gmail.com</li>
+      <li><span class="blue">Company:</span> ${data.company}</li>
+      <li><span class="blue">Website:</span> ${data.blog}</li>
+    </ul>
+  </div>
+
+  <div class="story">
+    <h1>The Story</h1>
+    <p>${data.bio}</p>
+  </div>
+    <img src=${data.avatar_url} alt="">
+  <div class="pic">
+  </section>
+  </div>`;
 
   $('.container').append(list)
 }
